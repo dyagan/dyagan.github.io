4f0ab80bdae08015f34bb72aeaf42460e854341b
clambygrum <green.c.clancy@gmail.com>
2018-09-13 11:51:55 -0700

    rebuilding site Thu Sep 13 11:51:55 PDT 2018

--- js/active.js
+++ js/active.js
@@ -1,20 +1,18 @@
-// Get the container element
-var btnContainer = document.getElementById("nav");
+$(function () {
+    setNavigation();
+});
 
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("btn");
+function setNavigation() {
+    var path = window.location.pathname;
+    path = decodeURIComponent(path);
 
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
+    console.log(path);
 
-    // If there's no active class
-    if (current.length > 0) { 
-      current[0].className = current[0].className.replace(" active", "");
-    }
-
-    // Add the active class to the current/clicked button
-    this.className += " activity";
-  });
+    $("#nav li a").each(function () {
+        var href = $(this).attr('href').replace("index.html", "");
+        if (path === href) {
+            $(this).addClass('current');
+            console.log(href);
+        }
+    });
 }
--- js/scroll.js
+++ js/scroll.js
@@ -1,16 +1,13 @@
 jQuery(document).ready(function($) {
 
-    /* ======= Scrollspy ======= */
-    $('body').scrollspy({ target: '#header', offset: 400});
-    
     /* ======= Fixed header when scrolled ======= */
     
     $(window).bind('scroll', function() {
          if ($(window).scrollTop() > 154) {
-             $('#header').addClass('navbar-fixed-top');
+             $('#nav').addClass('navbar-fixed-top');
          }
          else {
-             $('#header').removeClass('navbar-fixed-top');
+             $('#nav').removeClass('navbar-fixed-top');
          }
     });
 
